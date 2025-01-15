32f891da3671cdb8c9b07ff230d41ab315796921
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-01-15 23:23:01 +0500

    Create version-4.0.py

---
+++ project-5-word-counter/version-4.0.py
@@ -0,0 +1,28 @@
+print("Welcome! To Character Counter")
+print("Count the Characters of your words or sentence")
+
+while True:
+    user_word = input("Enter Your Word or type 'exit' to quit: ").strip().lower()
+
+    if user_word == "exit":
+        print("Goodbye! Thanks For Using")
+        break
+
+    char_count = {}
+
+    for i in user_word:
+        if i in char_count:
+            char_count.update({i:+i})
+        else:
+            char_count.update({i:1})    
+
+    if not char_count:
+        break       
+    else:
+        for key, value in char_count.items():
+            print(key, "=", value)
+
+    choice = input("Do you want to count more? (y/n): ").strip().lower()        
+    if choice != "y":
+        print("Goodbye! Thanks For Using")
+        break

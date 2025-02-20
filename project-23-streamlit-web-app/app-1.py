6d6624566c169705366ebb0f77b11880b1caf1f5
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-02-21 00:39:43 +0500

    Create app-1.py

---
+++ project-23-streamlit-web-app/app-1.py
@@ -0,0 +1,49 @@
+# Import necessary libraries
+import streamlit as st
+import pandas as pd
+import os
+from io import BytesIO
+
+# Set up our app
+st.set_page_config(page_title="💿 Data Sweeper", layout="wide")
+st.title("💿 Data Sweeper")
+st.write("Transform your files between CSV and Excel formats with built-in data cleaning and visualization!")
+
+uploaded_files = st.file_uploader("Upload your files (CSV or Excel):", type=["csv", "xlsx"], accept_multiple_files=True)
+
+if uploaded_files:
+    for file in uploaded_files:
+        file_ext = os.path.splitext(file.name)[-1].lower()
+
+        if file_ext == ".csv":
+            df = pd.read_csv(file)
+        elif file_ext == ".xlsx":
+            df = pd.read_excel(file)
+        else:
+            st.error(f"Unsupported file type: {file_ext}")
+            continue
+
+        # display file info
+        st.write(f"**File Name:** {file.name}")
+        st.write(f"**File Size:** {file.size / 1024}")
+
+        # show 5 rows of data frame
+        st.write("Preview the Head of the Data Frame")
+        st.write(df.head())
+
+        # Options for data cleaning
+        st.subheader("Data Cleaning Options")
+
+        if st.checkbox(f"Clean Data for {file.name}"):
+            col1, col2 = st.columns(2)
+
+            with col1:
+                if st.button(f"Remove Duplicates from {file.name}"):
+                    df.drop_duplicates(inplace=True)
+                    st.write("Duplicates Removed!")
+
+            with col2:
+                if st.button(f"Fill Missing Values from {file.name}"):
+                    numeric_cols = df.select_dtypes(include=["number"]).columns
+                    df[numeric_cols] = df[numeric_cols].fillna(df[numeric_cols].mean())
+                    st.write("Missing Values have been Filled!")
